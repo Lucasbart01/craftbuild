d0b4bd1b43c549d0c97e01c90966bafd19021e36
LucasBoni29 <santosboni2002@gmail.com>
2021-11-25 17:50:52 -0300

    Subindo a pagina CADASTRO

--- src/main/java/br/com/craftbuild/controller/UsuarioController.java
+++ src/main/java/br/com/craftbuild/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResultUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +22,13 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioDAO dao;
 
-	
+	//Chamando página de cadastro
+		@GetMapping("/cadastro")
+		public String cadastro() {
+		
+			return "index";
+		}
+		
 	//Chamando página de login
 	@GetMapping("/login")
 	public String index() {
@@ -47,6 +55,7 @@ public class UsuarioController {
 	        model.addAttribute("users", dao.findAll());
 	        return "area";
 	    }
+	 
 	
 	
 	
